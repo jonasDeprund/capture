363738ac15d532f18b2f8fa9e8e71059d47c73e9
Jonas Deprund <jonasdeprund@gmail.com>
2021-05-17 15:35:01 +0200

    faq added

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,13 +1,16 @@
 import React from 'react'
+
 // Import page components
 import AboutSection from '../components/AboutSection'
 import ServicesSection from '../components/ServicesSection'
+import FaqSection from '../components/FaqSection'
 
 const AboutUs = () => {
   return (
     <>
       <AboutSection />
       <ServicesSection />
+      <FaqSection />
     </>
   )
 }
