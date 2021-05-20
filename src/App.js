5c225d2a26b9167297a1102ab9b7e86f2b7a2e33
Jonas Deprund <jonasdeprund@gmail.com>
2021-05-20 18:40:34 +0200

    route added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,29 @@ import GlobalStyle from './components/GlobalStyle'
 
 // Import Pages
 import AboutUs from './pages/AboutUs'
+import ContactUs from './pages/ContactUs'
 import Nav from './components/Nav'
+import OurWork from './pages/OurWork'
+
+// router
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <Nav/>
-      <AboutUs />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,52 +1,57 @@
 // Import
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 const Nav = () => {
-    return(
-        <StyledNav>
-            <h1><a id='logo' href="#">Capture</a></h1>
-            <ul>
-                <li>
-                    <a href="#">1. About us</a>
-                </li>
-                <li>
-                    <a href="#">2. Our work</a>
-                </li>
-                <li>
-                    <a href="#">3. Contact Us</a>
-                </li>
-            </ul>
-        </StyledNav>
-    )
+  return (
+    <StyledNav>
+      <h1>
+        <Link id="logo" to="/">
+          Capture
+        </Link>
+      </h1>
+      <ul>
+        <li>
+          <Link to="/">1. About us</Link>
+        </li>
+        <li>
+          <Link to="/work">2. Our work</Link>
+        </li>
+        <li>
+          <Link to="/contact">3. Contact Us</Link>
+        </li>
+      </ul>
+    </StyledNav>
+  )
 }
 
 // Styled components
 const StyledNav = styled.nav`
-    min-height: 10vh;
+  min-height: 10vh;
+  display: flex;
+  margin: auto;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 10rem;
+  background-color: #282828;
+  a {
+    color: white;
+    text-decoration: none;
+  }
+  ul {
     display: flex;
-    margin: auto;
-    justify-content: space-between;
-    align-items: center;
-    padding: 1rem 10rem;
-    background-color: #282828;
-    a{
-        color: white;
-        text-decoration: none;
-    } 
-    ul{
-        display: flex;
-        list-style: none;
-    }
-    #logo{
-        font-size: 1.8rem;
-        font-family: 'lobster', cursive;
-        font-weight: lighter;
-    }
-    li{
-        padding-left: 10rem;
-        position: relative;
-    }
+    list-style: none;
+  }
+  #logo {
+    font-size: 1.8rem;
+    font-family: 'lobster', cursive;
+    font-weight: lighter;
+  }
+  li {
+    padding-left: 10rem;
+    position: relative;
+  }
 `
 
 export default Nav
