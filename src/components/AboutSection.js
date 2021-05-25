186b076fda98ba81b09b2925f4957c07bbe920d1
Jonas Deprund <jonasdeprund@gmail.com>
2021-05-25 22:02:12 +0200

    animation section done

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,6 +3,7 @@ import home1 from '../img/home1.png'
 
 // framer motion
 import { motion } from 'framer-motion'
+import { titleAnim, fade, photoAnim } from '../animation'
 
 // Styled Components
 import { About, Description, Image, Hide } from '../styles'
@@ -13,26 +14,26 @@ const AboutSection = () => {
       <Description>
         <motion.div>
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>true</motion.h2>
+            <motion.h2 variants={titleAnim}>true</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
           Curabitur blandit tempus porttitor. Maecenas sed diam eget risus
           varius blandit sit amet non magna.
-        </p>
-        <button>Contact us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="guy with a camera" />
+        <motion.img variants={photoAnim} src={home1} alt="guy with a camera" />
       </Image>
     </About>
   )
