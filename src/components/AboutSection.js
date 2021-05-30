efaa49671ffdda5a91259e929e6b4855551bc2a6
Jonas Deprund <jonasdeprund@gmail.com>
2021-05-30 22:39:06 +0200

    animations done

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -34,7 +34,13 @@ const AboutSection = () => {
         <motion.button variants={fade}>Contact us</motion.button>
       </Description>
       <Image>
-        <motion.img variants={photoAnim} src={home1} alt="guy with a camera" />
+        <motion.img
+          variants={photoAnim}
+          initial="hidden"
+          animate="show"
+          src={home1}
+          alt="guy with a camera"
+        />
       </Image>
       <Wave />
     </About>
