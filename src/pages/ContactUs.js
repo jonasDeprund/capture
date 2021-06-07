fb245cddc92f6ca2dcb25bc7911ea135e0c93798
Jonas Deprund <jonasdeprund@gmail.com>
2021-06-07 23:19:09 +0200

    wip contact animation

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -2,20 +2,40 @@ import React from 'react'
 
 // Animations
 import { motion } from 'framer-motion'
-import { pageAnimation } from '../animation'
+import { pageAnimation, titleAnim } from '../animation'
+import styled from 'styled-components'
 
 const ContactUs = () => {
   return (
-    <motion.div
+    <ContactStyle
       variants={pageAnimation}
       initial="hidden"
       animate="show"
       exit="exit"
       style={{ background: '#fff' }}
     >
-      <h1>Contact us</h1>
-    </motion.div>
+      <Title>
+        <Hide>
+          <motion.h2 variants={titleAnim}>Get in touch</motion.h2>
+        </Hide>
+      </Title>
+    </ContactStyle>
   )
 }
+
+const ContactStyle = styled(motion.div)`
+  padding: 5rem 10rem;
+  color: #353535;
+  min-height: 90vh;
+`
+
+const Title = styled.div`
+  margin-bottom: 4rem;
+  color: black;
+`
+
+const Hide = styled.div`
+  overflow: hidden;
+`
 
 export default ContactUs
