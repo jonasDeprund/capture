d6c05a98fd19addee3a6d510618d1a8e54472fff
Jonas Deprund <jonasdeprund@gmail.com>
2021-06-08 21:33:51 +0200

    social done

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -19,6 +19,14 @@ const ContactUs = () => {
           <motion.h2 variants={titleAnim}>Get in touch</motion.h2>
         </Hide>
       </Title>
+      <Hide>
+        <div>
+          <Social variants={titleAnim}>
+            <Circle />
+            <h2>Send us a message</h2>
+          </Social>
+        </div>
+      </Hide>
     </ContactStyle>
   )
 }
@@ -36,6 +44,20 @@ const Title = styled.div`
 
 const Hide = styled.div`
   overflow: hidden;
+`
+const Circle = styled.div`
+  border-radius: 50%;
+  width: 3rem;
+  height: 3rem;
+  background: #353535;
+`
+
+const Social = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  h2 {
+    margin: 2rem;
+  }
 `
 
 export default ContactUs
